e518b122a1bce97bde55a490cfad3e541c87bbf3
PedrassoxD <pedroperlab@gmail.com>
2019-10-03 10:11:28 +0200

    Nueva revision

--- Despertador_Temporizador/JavaScript.js
+++ Despertador_Temporizador/JavaScript.js
@@ -8,31 +8,42 @@
         let tiempo = d.toLocaleTimeString();
     
         document.getElementById("fecha").innerHTML = "Dia " + dia + " Mes " + mes + " Año " + ano + " Hora " + tiempo;
+        document.getElementById("reloj").innerHTML = tiempo;
     }
 
 
-    const time2 = setInterval(ponerTiempo, 1000);
+    
 
-    function ponerTiempo(){
-        let d = new Date();
-        let tiempo = d.toLocaleTimeString();
-        document.getElementById("reloj").innerHTML = tiempo;
+    const boton = document.getElementById("boton");
 
-        let hora = document.getElementById("hora").value;
-        let minuto = document.getElementById("minuto").value;
-        let segundo = d.getSeconds();
+    boton.addEventListener("click",()=>{
 
-        if(hora == d.getHours() && minuto == d.getMinutes() && segundo == 00){
+        const time2 = setInterval(ponerTiempo, 1000);
 
-            let confirma = confirm("Es la hora de despertarse!! Son las " + hora + ":" + minuto);
-
-                if(confirma == true){
-                    setTimeout(() => {
-                        alert("Illo que te levantes socio!!!");
-                    }, 120000);
-                }else{
-                    alert("A TRABAJAAR!!!");
-                }
+        function ponerTiempo(){
+            let d = new Date();
+            let tiempo = d.toLocaleTimeString();
+    
+            let hora = document.getElementById("hora").value;
+            let minuto = document.getElementById("minuto").value;
+            let segundo = d.getSeconds();
+    
+            if(hora == d.getHours() && minuto == d.getMinutes() && segundo == 00){
+    
+                let confirma = confirm("Es la hora de despertarse!! Son las " + hora + ":" + minuto);
+    
+                    if(confirma == true){
+                        setTimeout(() => {
+                            alert("Illo que te levantes sosio!!!");
+                        }, 120000);
+                    }else{
+                        alert("A TRABAJAAR!!!");
+                    }
+            }
+    
         }
 
-    }
+    });
+
+
+    
