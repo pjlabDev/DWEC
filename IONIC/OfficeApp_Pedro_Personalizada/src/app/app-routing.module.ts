cbdd79616df576bf64deb179f2d4ef9db25cd6fc
PedrassoxD <pedroperlab@gmail.com>
2019-12-05 09:38:23 +0100

    New revision

---
+++ IONIC/OfficeApp_Pedro_Personalizada/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'materials',
+    children:[
+      {
+        path: '',
+        loadChildren: () => import('./materials/materials.module').then( m => m.MaterialsPageModule)
+      },
+      {
+        path: 'detail/:materialId',
+        loadChildren: () => import('./material-detail/material-detail.module').then( m => m.MaterialDetailPageModule)
+      },
+      {
+        path: 'edit/:materialId',
+        loadChildren: () => import('./material-edit/material-edit.module').then( m => m.MaterialEditPageModule)
+      },
+      {
+        path: 'buy/:materialId',
+        loadChildren: () => import('./material-buy/material-buy.module').then( m => m.MaterialBuyPageModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/materials',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
